4d129f2ac9595fd6148e86c472c25accf696cfdd
Yehowshua Immanuel <yimmanuel3@gatech.edu>
2020-06-30 02:09:20 -0400

    Abstraction Overhual

--- demo/frontend/frontend.js
+++ demo/frontend/frontend.js
@@ -1,28 +1,7 @@
-function tick(ev) {
-    ev.preventDefault();
-    fetch("/tick", {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-    })
-  }
-
-  function repaint(){
-    const payload = fetch("/update", {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({op:"read_updates"})
-    })
-    .then((response) => {return response.json()})
-    .then((payload) => {
+  function do_update(payload){
       // update page elements
       var ticks = document.getElementById('ticks');
       ticks.innerText = payload.ticks;
       var result = document.getElementById('result');
       result.innerText = payload.state;
-    })
-
   }
-
-  var form = document.getElementById('calc');
-  form.addEventListener('submit', tick);
-  setInterval(repaint, 50);
